15daec986be0b1cee4e7424b178abb55b24a4538
Christopher Hill <chill.signup@gmail.com>
2021-10-02 20:31:33 +0200

    Update Custom-Extension.ps1

--- Custom-Extension.ps1
+++ Custom-Extension.ps1
@@ -1,16 +1,54 @@
-$Password_sec = (ConvertTo-SecureString -String $($password) -AsPlainText -Force)
-New-LocalUser $($userName) -Password $($Password_sec)
-Add-LocalGroupMember -Group "Administrators" -Member $($userName)
-Set-LocalUser -Name $($userName) -PasswordNeverExpires 1
+param (
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$Password,
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$UserName,
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$ServiceName
+)
+
+function Set-ServiceCredentials {
+    param (
+        [String]$password,
+        [String]$userName,
+        [String]$serviceName
+    )
+    process {
+
+        Stop-Service -Name $($serviceName)
+        cmd /c sc config $Service obj= "$($env:COMPUTERNAME)\$($username)" password= $Password
+        Start-Service -Name $($serviceName) 
+    }
+}
+
+function New-LocalUser {
+    param (
+        [String]$password,
+        [String]$userName,
+    )
+    process {
+        $passwordSec = (ConvertTo-SecureString -String $($password) -AsPlainText -Force)
+        New-LocalUser $($userName) -Password $($passwordSec)
+        Add-LocalGroupMember -Group "Administrators" -Member $($userName)
+        Set-LocalUser -Name $($userName) -PasswordNeverExpires 1
+    }
+}
+
+function New-LocalUser {
+    param (
+        [String]$userName,
+    )
+    begin {
+        Install-Module -Name 'Carbon' -AllowClobber
+        Import-Module 'Carbon'
+    }    
+    process {
+        $privilege = "SeServiceLogonRight"
+        $CarbonDllPath = "C:\Program Files\WindowsPowerShell\Modules\Carbon\2.10.2\bin\fullclr\Carbon.dll"
+        [Reflection.Assembly]::LoadFile($CarbonDllPath)
+        [Carbon.Security.Privilege]::GrantPrivileges($userName, $privilege)
+    }
+}
+
+set-serviceCredentials -password $Password -userName $UserName -serviceName $ServiceName
 
 
-Install-Module -Name 'Carbon' -AllowClobber
-Import-Module 'Carbon'
-$privilege = "SeServiceLogonRight"
-$CarbonDllPath = "C:\Program Files\WindowsPowerShell\Modules\Carbon\2.10.2\bin\fullclr\Carbon.dll"
-[Reflection.Assembly]::LoadFile($CarbonDllPath)
-[Carbon.Security.Privilege]::GrantPrivileges($username, $privilege)
 
-Stop-Service -Name $Service
-cmd /c sc config $Service obj= "$($env:COMPUTERNAME)\$($username)" password= $Password
-Start-Service -Name $Service 
+
